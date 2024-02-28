d373f0d0b67442884c35758b76a6e49c1f374930
Lukas Geyer <geyer.lukas@web.de>
2024-02-28 16:10:16 +0100

    introduced functions for prediction analysis

--- src/LmaPredict.jl
+++ src/LmaPredict.jl
@@ -1,9 +1,10 @@
 module LmaPredict
 
 include("DataIO/DataIO.jl")
+include("PredictionAnalysis/PredictionAnalysis.jl")
 
-using .DataIO
+using .DataIO, .PredictionAnalysis
 export LMAConfig
-export read_eigen_vals, read_eigen_eigen, read_rest_rest, read_rest_eigen, get_LMAConfig
+export read_eigen_vals, read_eigen_eigen, read_rest_rest, read_rest_eigen, get_LMAConfig, analyse_predictions
 
 end # module
---
+++ src/PredictionAnalysis/PredictionAnalysis.jl
@@ -0,0 +1,9 @@
+module PredictionAnalysis
+
+using StatsBase, Plots, Flux, PrettyTables, Random
+
+include("Analysis.jl")
+    
+export analyse_predictions
+
+end
